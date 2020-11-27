c10e6e0cb2f337c370e0da9c36e8e174b5017156
Thabiso Pheko <tabs150@gmail.com>
2020-11-27 22:22:52 +0200

    Add a filter component

---
+++ components/ProductCard.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, CardContent, CardMedia, Typography } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
+import { makeStyles } from '@material-ui/core/styles';
+import Rating from './Rating';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 280,
+    position: 'relative',
+  },
+  media: {
+    height: 160,
+    position: 'relative',
+    overflow: 'hidden',
+    cursor: 'pointer',
+  },
+  contentAlign: {
+    textAlign: 'center',
+  },
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    height: 160,
+    width: '100%',
+    zIndex: 1,
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    textAlign: 'center',
+    padding: theme.spacing(4),
+    backgroundColor: 'rgba(255,255,255,.8)',
+    opacity: 0,
+    transition: 'all 0.3s ease-in-out',
+    '&:hover': {
+      top: 0,
+      opacity: 0.9,
+    },
+  },
+  iconRoot: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: 'auto',
+    borderWidth: '2px',
+    borderStyle: 'solid',
+  },
+}));
+
+export default function ProductCard({ product }) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardMedia
+        className={classes.media}
+        image={product.image}
+        title={product.name}
+      />
+      <div className={classes.overlay}>
+        <IconButton className={classes.iconRoot} size='small' color='primary'>
+          <FavoriteBorderOutlinedIcon />
+        </IconButton>
+        <IconButton
+          className={classes.iconRoot}
+          size='small'
+          color='primary'
+        >
+          <ShoppingCartOutlinedIcon />
+        </IconButton>
+      </div>
+      <CardContent className={classes.contentAlign}>
+        <Typography variant='h6' component='h2'>
+          {product.name}
+        </Typography>
+        <Rating rating={product.rating} />
+        <Typography component='p'>${product.price}</Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+ProductCard.propTypes = {
+  product: PropTypes.object,
+  onAddToCart: PropTypes.func,
+};
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import ProductFilter from '../components/ProductFilter';
+
+const useStyles = makeStyles((theme) => ({
+  grid: {
+    width: '80%',
+    margin: 'auto',
+  },
+  section: {
+    padding: theme.spacing(8, 0),
+  },
+  sectionTitle: {
+    flex: 1,
+    height: '4rem',
+    alignItems: 'center',
+    textTransform: 'uppercase',
+  },
+  topProducts: {
+    margin: '-100px auto auto',
+  },
+}));
 
 function Home() {
+  const classes = useStyles();
   return (
     <>
-    <h1>This is home</h1>
+      <section id='product-filter' className={classes.section}>
+        <ProductFilter />
+      </section>
     </>
   )
 }
